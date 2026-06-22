feb62dbd37b506e796d0cf86d560005354f468a9
nupurmita2411 <khandokernupur459@gmail.com>
2026-06-22 14:23:41 +0600

    Delete src directory
    delete

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.summer26.section1.group1.diamondworld {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.summer26.section1.group1.diamondworld to javafx.fxml;
-    exports com.example.summer26.section1.group1.diamondworld;
-}
